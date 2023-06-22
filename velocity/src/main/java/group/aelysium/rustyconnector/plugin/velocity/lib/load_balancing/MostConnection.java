c464918b9b4707fe37fc47a91fc2ec4517a46553
TheDeafCreeper <34897909+TheDeafCreeper@users.noreply.github.com>
2023-06-22 01:59:28 -0500

    Made MOST_CONNECTION iterator only iterate when server full.

--- velocity/src/main/java/group/aelysium/rustyconnector/plugin/velocity/lib/load_balancing/MostConnection.java
+++ velocity/src/main/java/group/aelysium/rustyconnector/plugin/velocity/lib/load_balancing/MostConnection.java
@@ -2,10 +2,23 @@ package group.aelysium.rustyconnector.plugin.velocity.lib.load_balancing;
 
 import group.aelysium.rustyconnector.core.lib.algorithm.QuickSort;
 import group.aelysium.rustyconnector.core.lib.algorithm.WeightedQuickSort;
+import group.aelysium.rustyconnector.plugin.velocity.lib.server.PlayerServer;
 
 import java.util.Collections;
 
-public class MostConnection extends LeastConnection {
+public class MostConnection extends LoadBalancer {
+
+    @Override
+    public void iterate() {
+        try {
+            PlayerServer thisItem = this.items.get(this.index);
+
+            if (thisItem.getPlayerCount() + 1 >= thisItem.getHardPlayerCap()) this.index++;
+            if(this.index >= this.items.size()) this.index = 0;
+        } catch (IndexOutOfBoundsException ignore) {}
+    }
+
+    
     @Override
     public void completeSort() {
         this.index = 0;
