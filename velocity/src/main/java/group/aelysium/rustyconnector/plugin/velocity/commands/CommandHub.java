ec4ffeb925e17920534d8a79db5bce2e417c38d3
TheDeafCreeper <34897909+TheDeafCreeper@users.noreply.github.com>
2023-07-04 02:23:30 -0500

    Made /hub utilize parents of parents instead of just sending to root on failiure.
    This may be more complex than it needs to be, but :shrug:

--- velocity/src/main/java/group/aelysium/rustyconnector/plugin/velocity/commands/CommandHub.java
+++ velocity/src/main/java/group/aelysium/rustyconnector/plugin/velocity/commands/CommandHub.java
@@ -18,7 +18,10 @@ import group.aelysium.rustyconnector.plugin.velocity.lib.lang_messaging.Velocity
 import group.aelysium.rustyconnector.plugin.velocity.lib.server.PlayerServer;
 import group.aelysium.rustyconnector.plugin.velocity.lib.server.ServerService;
 import net.kyori.adventure.text.Component;
+import net.kyori.adventure.text.format.NamedTextColor;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 
 public class CommandHub {
@@ -31,63 +34,82 @@ public class CommandHub {
                 .requires(source -> source instanceof Player)
                 .executes(context -> {
                     if(!(context.getSource() instanceof Player player)) {
-                        logger.log("/hub must be sent as a player!");
+                        VelocityLang.BOXED_MESSAGE_COLORED.send(
+                                logger,
+                                Component.text("/hub must be sent as a player!"),
+                                NamedTextColor.RED
+                        );
+
                         return Command.SINGLE_SUCCESS;
                     }
 
                     ServerInfo sendersServerInfo = ((Player) context.getSource()).getCurrentServer().orElseThrow().getServerInfo();
 
                     PlayerServer sendersServer = api.getService(ServerService.class).findServer(sendersServerInfo);
-
                     BaseServerFamily senderFamily = sendersServer.getFamily();
                     ScalarServerFamily rootFamily = api.getService(FamilyService.class).getRootFamily();
 
                     if (!(senderFamily instanceof PlayerFocusedServerFamily)) {
                         // Attempt to connect to root family if we're not in a PlayerFocusedServerFamily
-                        if (rootFamily != null) {
-                            try {
-                                ((PlayerFocusedServerFamily) rootFamily).connect(player);
-                                return Command.SINGLE_SUCCESS;
-                            } catch (RuntimeException err) {
-                                VelocityLang.RC_FAMILY_ERROR.send(logger, "Failed to connect player to parent family!");
-                                context.getSource().sendMessage(Component.text("Failed to connect to root family!"));
-                            }
-                        } else {
-                            VelocityLang.RC_FAMILY_ERROR.send(logger, "You can only scalar and static families for family parents!");
-                            context.getSource().sendMessage(Component.text("Failed to connect to root family!"));
+                        try {
+                            ((PlayerFocusedServerFamily) rootFamily).connect(player);
+                            return Command.SINGLE_SUCCESS;
+                        } catch (RuntimeException err) {
+                            VelocityLang.RC_SEND_NO_SERVER.send(logger, "Failed to connect player to parent family " + rootFamily.getName() + "!");
+                            context.getSource().sendMessage(Component.text("Failed to connect you to any parent servers!"));
                         }
 
                         return Command.SINGLE_SUCCESS;
                     }
 
-                    String parentFamilyName = ((PlayerFocusedServerFamily) senderFamily).getParentFamily();
-                    BaseServerFamily parentFamily;
+                    // Compile a list of parent families, going up the chain until
+                    //  it either loops, or there are no more parents.
+                    // Mostly just because it's a bit nicer than just being sent
+                    // to the root family if the first parent is down.
+                    List<PlayerFocusedServerFamily> parentFamilies = new ArrayList<>();
 
-                    if (Objects.equals(parentFamilyName, "")) parentFamily = rootFamily;
-                    else parentFamily = api.getService(FamilyService.class).find(parentFamilyName);
+                    String currentFamilyName = ((PlayerFocusedServerFamily) senderFamily).getParentFamily();
 
-                    // Attempt to connect to parent family.
-                    if (parentFamily instanceof PlayerFocusedServerFamily) {
+                    int maxDepth = 10;
+                    while (maxDepth > 0) {
+                        BaseServerFamily parentFamily;
+
+                        // If there's no parent, we're done. Otherwise, fetch the parent.
+                        if (Objects.equals(currentFamilyName, "")) break;
+                        else parentFamily = api.getService(FamilyService.class).find(currentFamilyName);
+
+                        // If the parent is not a PlayerFocusedServerFamily, we can't go any further.
+                        if (!(parentFamily instanceof PlayerFocusedServerFamily)) break;
+
+                        // If we've already added this parent to the list we're in a loop, stop here.
+                        // Otherwise, add it to the list and move on.
+                        if (parentFamilies.contains(parentFamily)) break;
+                        else parentFamilies.add((PlayerFocusedServerFamily) parentFamily);
+
+                        // Update the current family name for the next iteration.
+                        currentFamilyName = ((PlayerFocusedServerFamily) parentFamily).getParentFamily();
+
+                        maxDepth--;
+                    }
+
+                    // Add root family as last resort.
+                    parentFamilies.add(rootFamily);
+
+                    boolean playerSent = false;
+
+                    for (PlayerFocusedServerFamily family : parentFamilies) {
                         try {
-                            ((PlayerFocusedServerFamily) parentFamily).connect(player);
-                            return Command.SINGLE_SUCCESS;
+                            family.connect(player);
+                            playerSent = true;
+                            break;
                         } catch (RuntimeException err) {
-                            VelocityLang.RC_FAMILY_ERROR.send(logger, "Failed to connect player to parent family!");
+                            VelocityLang.RC_SEND_NO_SERVER.send(logger, "Failed to connect player to parent family " + family.getName() + "!");
                         }
-                    } else VelocityLang.RC_FAMILY_ERROR.send(logger,"You can only set scalar and static families for family parents!");
+                    }
 
-                    // Attempt to connect to root family if that fails.
-                    if (rootFamily != null) {
-                        try {
-                            ((PlayerFocusedServerFamily) rootFamily).connect(player);
-                            return Command.SINGLE_SUCCESS;
-                        } catch (RuntimeException err) {
-                            VelocityLang.RC_FAMILY_ERROR.send(logger, "Failed to connect player to parent family!");
-                            context.getSource().sendMessage(Component.text("Failed to connect to both parent and root family!"));
-                        }
-                    } else {
-                        VelocityLang.RC_FAMILY_ERROR.send(logger, "You can only scalar and static families for family parents!");
-                        context.getSource().sendMessage(Component.text("Failed to connect to both parent and root family!"));
+                    // May as well let the player know if the send failed.
+                    if (!playerSent) {
+                        player.sendMessage(Component.text("Failed to connect you to any parent servers!"));
                     }
 
                     return Command.SINGLE_SUCCESS;
