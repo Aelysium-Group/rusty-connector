cc1e8931bd099fd5400e1550fddf566d82acba9c
Nathan Martin <63524975+nathan-i-martin@users.noreply.github.com>
2023-09-01 17:05:49 -0400

    Add connectors.yml and update config version

--- plugin/velocity/src/main/java/group/aelysium/rustyconnector/plugin/velocity/config/YAML.java
+++ plugin/velocity/src/main/java/group/aelysium/rustyconnector/plugin/velocity/config/YAML.java
@@ -11,7 +11,7 @@ import java.io.InputStream;
 import java.nio.file.Files;
 
 public class YAML extends group.aelysium.rustyconnector.core.lib.config.YAML {
-    protected static int currentVersion = 4;
+    protected static int currentVersion = 5;
     public YAML(File configPointer, String template) {
         super(configPointer, template);
     }
