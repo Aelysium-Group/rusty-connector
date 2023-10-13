6425d246021d3f8c34083e85c6b1581c14d819d4
Nathan Martin <63524975+nathan-i-martin@users.noreply.github.com>
2023-10-13 13:35:16 -0400

    Rework some stuff

--- plugin/velocity/src/main/java/group/aelysium/rustyconnector/plugin/velocity/lib/k8/K8Service.java
+++ plugin/velocity/src/main/java/group/aelysium/rustyconnector/plugin/velocity/lib/k8/K8Service.java
@@ -5,24 +5,23 @@ import group.aelysium.rustyconnector.plugin.velocity.lib.server.K8PlayerServer;
 import io.kubernetes.client.openapi.ApiClient;
 import io.kubernetes.client.openapi.ApiException;
 import io.kubernetes.client.openapi.Configuration;
+import io.kubernetes.client.openapi.apis.BatchV1Api;
 import io.kubernetes.client.openapi.apis.CoreV1Api;
-import io.kubernetes.client.openapi.models.V1DeleteOptions;
-import io.kubernetes.client.openapi.models.V1ObjectMeta;
-import io.kubernetes.client.openapi.models.V1Pod;
+import io.kubernetes.client.openapi.models.*;
 import io.kubernetes.client.util.Config;
 
 import java.io.IOException;
 
 public class K8Service extends Service {
     private ApiClient client;
-    private CoreV1Api api;
+    private BatchV1Api api;
     private K8AutoScalerService autoScalerService;
 
     public K8Service() throws IOException {
         this.client = Config.defaultClient();
         Configuration.setDefaultApiClient(this.client);
 
-        this.api = new CoreV1Api();
+        this.api = new BatchV1Api();
     }
 
     public K8AutoScalerService autoscalers() {
@@ -30,33 +29,27 @@ public class K8Service extends Service {
     }
 
     public void createServer(String namespace) throws ApiException {
-        V1Pod pod = new V1Pod();
+        {
+            V1Pod pod = new V1Pod();
+            V1ObjectMeta meta = new V1ObjectMeta();
+            meta.name("rc-minecraft-" + namespace + "-");
+            meta.deletionGracePeriodSeconds(0L);
+            pod.metadata(meta);
+        }
+
+        V1Job job = new V1Job();
         V1ObjectMeta meta = new V1ObjectMeta();
         meta.name("rc-minecraft-"+namespace+"-");
         meta.deletionGracePeriodSeconds(0L);
-        pod.metadata(meta);
+        job.metadata(meta);
 
-        this.api.createNamespacedPod(
-                namespace,
-                pod,
-                null,
-                null,
-                null,
-                null
-        );
+        V1JobSpec spec = new V1JobSpec();
+        job.spec();
+
+        this.api.createNamespacedJob(namespace, null, null, null, null, null);
     }
 
     public void deleteServer(K8PlayerServer server) throws ApiException {
-        this.api.deleteNamespacedPod(
-                server.family().name(),
-                server.podName(),
-                null,
-                null,
-                0,
-                true,
-                null,
-                new V1DeleteOptions()
-        );
     }
 
     @Override
