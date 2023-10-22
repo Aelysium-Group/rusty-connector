5ff5a94a54a6949aee24d8e876dd11b99dafa86c
Nathan Martin <63524975+nathan-i-martin@users.noreply.github.com>
2023-10-22 18:38:41 -0400

    Implement some basic K8 support

--- plugin/velocity/src/main/java/group/aelysium/rustyconnector/plugin/velocity/lib/k8/K8Service.java
+++ plugin/velocity/src/main/java/group/aelysium/rustyconnector/plugin/velocity/lib/k8/K8Service.java
@@ -1,55 +1,70 @@
 package group.aelysium.rustyconnector.plugin.velocity.lib.k8;
 
 import group.aelysium.rustyconnector.core.lib.serviceable.Service;
+import group.aelysium.rustyconnector.plugin.velocity.lib.family.bases.BaseServerFamily;
 import group.aelysium.rustyconnector.plugin.velocity.lib.server.K8PlayerServer;
 import io.kubernetes.client.openapi.ApiClient;
 import io.kubernetes.client.openapi.ApiException;
 import io.kubernetes.client.openapi.Configuration;
-import io.kubernetes.client.openapi.apis.BatchV1Api;
 import io.kubernetes.client.openapi.apis.CoreV1Api;
 import io.kubernetes.client.openapi.models.*;
 import io.kubernetes.client.util.Config;
 
 import java.io.IOException;
+import java.util.List;
 
 public class K8Service extends Service {
     private ApiClient client;
-    private BatchV1Api api;
+    private final CoreV1Api api;
     private K8AutoScalerService autoScalerService;
 
     public K8Service() throws IOException {
         this.client = Config.defaultClient();
         Configuration.setDefaultApiClient(this.client);
 
-        this.api = new BatchV1Api();
+        this.api = new CoreV1Api();
     }
 
     public K8AutoScalerService autoscalers() {
         return this.autoScalerService;
     }
 
-    public void createServer(String namespace) throws ApiException {
-        {
-            V1Pod pod = new V1Pod();
-            V1ObjectMeta meta = new V1ObjectMeta();
-            meta.name("rc-minecraft-" + namespace + "-");
-            meta.deletionGracePeriodSeconds(0L);
-            pod.metadata(meta);
-        }
+    public void createServer(BaseServerFamily<?> family, String containerName, int containerPort) throws ApiException {
+        String podName = familyNameToPodPrefix(family.name());
+        String namespace = familyNameToNamespace(family.name());
 
-        V1Job job = new V1Job();
         V1ObjectMeta meta = new V1ObjectMeta();
-        meta.name("rc-minecraft-"+namespace+"-");
-        meta.deletionGracePeriodSeconds(0L);
-        job.metadata(meta);
+        meta.name(podName);
 
-        V1JobSpec spec = new V1JobSpec();
-        job.spec();
+        V1ContainerPort port = new V1ContainerPort();
+        port.setContainerPort(containerPort);
 
-        this.api.createNamespacedJob(namespace, null, null, null, null, null);
+        V1Container container = new V1Container();
+        container.setName(containerName);
+        container.setImage("nginx:latest");
+        container.setPorts(List.of(port));
+
+        V1PodSpec spec = new V1PodSpec();
+        spec.setContainers(List.of(container));
+
+        V1Pod pod = new V1Pod();
+        pod.setMetadata(meta);
+        pod.setSpec(spec);
+
+        api.createNamespacedPod(namespace, pod, null, null, null, null);
     }
 
     public void deleteServer(K8PlayerServer server) throws ApiException {
+        String namespace = familyNameToNamespace(server.family().name());
+        api.deleteNamespacedPod(server.podName(), namespace, null, null, null, null, null, null);
+    }
+
+    private String familyNameToNamespace(String name) {
+        return "rc-minecraft-family-"+name;
+    }
+
+    private String familyNameToPodPrefix(String name) {
+        return "rc-minecraft-pod-"+name+"-";
     }
 
     @Override
