a68277ef2ed50dcb1d3e49d96d2694f1a40e833b
TheDeafCreeper <34897909+TheDeafCreeper@users.noreply.github.com>
2024-01-09 13:48:00 -0600

    Fixed 1 more incorrect lang node

--- plugin/velocity/src/main/java/group/aelysium/rustyconnector/plugin/velocity/lib/lang/ProxyLang.java
+++ plugin/velocity/src/main/java/group/aelysium/rustyconnector/plugin/velocity/lib/lang/ProxyLang.java
@@ -801,7 +801,7 @@ public class ProxyLang extends Lang {
     public final static ParameterizedMessage1<String> PARTY_INVITE_TARGET_NOT_ONLINE = (username) ->
             resolver().get("proxy.party.invite.target_not_online", LanguageResolver.tagHandler("username", username));
     public final static ParameterizedMessage1<String> PARTY_JOINED = (username) ->
-            resolver().get("velocity.party.party_joined", LanguageResolver.tagHandler("username", username));
+            resolver().get("proxy.party.party_joined", LanguageResolver.tagHandler("username", username));
     public final static Component PARTY_FOLLOWING_KICKED_GENERIC = resolver().get("proxy.party.following_kicked.generic");
     public final static Component PARTY_FOLLOWING_KICKED_SERVER_FULL = resolver().get("proxy.party.following_kicked.server_full");
     public final static Component PARTY_FOLLOWING_FAILED_GENERIC = resolver().get("proxy.party.following_failed.generic");
