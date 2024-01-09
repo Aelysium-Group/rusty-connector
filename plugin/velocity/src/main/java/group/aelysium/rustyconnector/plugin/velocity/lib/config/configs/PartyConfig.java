de260d262cf1104c956882f5710e09331f404e87
TheDeafCreeper <34897909+TheDeafCreeper@users.noreply.github.com>
2024-01-09 13:44:33 -0600

    Fix config node path

--- plugin/velocity/src/main/java/group/aelysium/rustyconnector/plugin/velocity/lib/config/configs/PartyConfig.java
+++ plugin/velocity/src/main/java/group/aelysium/rustyconnector/plugin/velocity/lib/config/configs/PartyConfig.java
@@ -106,7 +106,7 @@ public class PartyConfig extends YAML implements group.aelysium.rustyconnector.t
             throw new IllegalStateException("Switch power: "+this.switchingServers_switchPower+" isn't valid! Please review `party.yml` again!");
         }
 
-        this.switchingServers_kickOnSendFailure = IYAML.getValue(this.data, "servers.kick-on-failure", Boolean.class);
+        this.switchingServers_kickOnSendFailure = IYAML.getValue(this.data, "switching-servers.kick-on-failure", Boolean.class);
     }
 
     public static PartyConfig construct(Path dataFolder, LangService lang, ConfigService configService) {
