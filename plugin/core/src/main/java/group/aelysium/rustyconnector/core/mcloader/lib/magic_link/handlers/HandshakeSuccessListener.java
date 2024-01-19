7a6bb92327084371e4bea28d4f32c330adbdfa44
Nathan Martin <63524975+nathan-i-martin@users.noreply.github.com>
2024-01-19 10:27:16 -0500

    Fix Ranked Game Interface issue

--- plugin/core/src/main/java/group/aelysium/rustyconnector/core/mcloader/lib/magic_link/handlers/HandshakeSuccessListener.java
+++ plugin/core/src/main/java/group/aelysium/rustyconnector/core/mcloader/lib/magic_link/handlers/HandshakeSuccessListener.java
@@ -5,11 +5,13 @@ import group.aelysium.rustyconnector.core.lib.packets.BuiltInIdentifications;
 import group.aelysium.rustyconnector.core.lib.packets.MagicLink;
 import group.aelysium.rustyconnector.core.mcloader.central.MCLoaderTinder;
 import group.aelysium.rustyconnector.core.mcloader.lib.lang.MCLoaderLang;
+import group.aelysium.rustyconnector.core.mcloader.lib.ranked_game_interface.RankedGameInterfaceService;
 import group.aelysium.rustyconnector.toolkit.core.logger.PluginLogger;
 import group.aelysium.rustyconnector.toolkit.core.packet.Packet;
 import group.aelysium.rustyconnector.toolkit.core.packet.PacketIdentification;
 import group.aelysium.rustyconnector.toolkit.core.packet.PacketListener;
 import group.aelysium.rustyconnector.core.mcloader.lib.magic_link.MagicLinkService;
+import group.aelysium.rustyconnector.toolkit.core.server.ServerAssignment;
 import group.aelysium.rustyconnector.toolkit.mc_loader.events.magic_link.ConnectedEvent;
 import group.aelysium.rustyconnector.toolkit.mc_loader.events.magic_link.DisconnectedEvent;
 import net.kyori.adventure.text.Component;
@@ -42,5 +44,7 @@ public class HandshakeSuccessListener extends PacketListener<MagicLink.Handshake
 
         service.setDelay(packet.pingInterval());
         api.services().serverInfo().assignment(packet.assignment());
+
+        if(packet.assignment() == ServerAssignment.RANKED_GAME_SERVER) api.services().add(new RankedGameInterfaceService());
     }
 }
