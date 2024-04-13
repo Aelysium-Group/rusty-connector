1d4f217a5f938a814aeb7e324d60fa9c14195df8
Nathan Martin <nathan@noknt.net>
2024-04-12 23:51:32 -0400

    Make resolver use IPlayerRank.schema

--- plugin/velocity/src/main/java/group/aelysium/rustyconnector/plugin/velocity/lib/matchmaking/rank/DefaultRankResolver.java
+++ plugin/velocity/src/main/java/group/aelysium/rustyconnector/plugin/velocity/lib/matchmaking/rank/DefaultRankResolver.java
@@ -12,30 +12,29 @@ public class DefaultRankResolver implements IRankResolver {
     public IPlayerRank resolve(JsonObject object) throws IllegalStateException {
         String schema = object.get("schema").getAsString();
 
-        return switch (schema) {
-            case "RANDOMIZE" -> RandomizedPlayerRank.New();
-            case "WIN_LOSS" -> {
-                int wins = object.get("wins").getAsInt();
-                int losses = object.get("losses").getAsInt();
-                yield new WinLossPlayerRank(wins, losses);
-            }
-            case "WIN_RATE" -> {
-                int wins = object.get("wins").getAsInt();
-                int losses = object.get("losses").getAsInt();
-                int ties = object.get("ties").getAsInt();
-                yield new WinRatePlayerRank(wins, losses, ties);
-            }
-            case "ELO" -> {
-                double elo = object.get("elo").getAsDouble();
-                yield new ELOPlayerRank(elo);
-            }
-            case "OPEN_SKILL" -> {
-                double mu = object.get("mu").getAsDouble();
-                double sigma = object.get("sigma").getAsDouble();
-                yield new OpenSkillPlayerRank(mu, sigma);
-            }
-            default -> throw new IllegalStateException("The passed rank object uses schema "+schema+" which doesn't match a supported schema!");
-        };
+        if(schema.equals(RandomizedPlayerRank.schema()))
+            return RandomizedPlayerRank.New();
+        if(schema.equals(WinLossPlayerRank.schema())) {
+            int wins = object.get("wins").getAsInt();
+            int losses = object.get("losses").getAsInt();
+            return new WinLossPlayerRank(wins, losses);
+        }
+        if(schema.equals(WinRatePlayerRank.schema())) {
+            int wins = object.get("wins").getAsInt();
+            int losses = object.get("losses").getAsInt();
+            int ties = object.get("ties").getAsInt();
+            return new WinRatePlayerRank(wins, losses, ties);
+        }
+        if(schema.equals(ELOPlayerRank.schema())) {
+            double elo = object.get("elo").getAsDouble();
+            return new ELOPlayerRank(elo);
+        }
+        if(schema.equals(OpenSkillPlayerRank.schema())) {
+            double mu = object.get("mu").getAsDouble();
+            double sigma = object.get("sigma").getAsDouble();
+            return new OpenSkillPlayerRank(mu, sigma);
+        }
+        throw new IllegalStateException("The passed rank object uses schema "+schema+" which doesn't match a supported schema!");
     }
 
     public static DefaultRankResolver New() {
