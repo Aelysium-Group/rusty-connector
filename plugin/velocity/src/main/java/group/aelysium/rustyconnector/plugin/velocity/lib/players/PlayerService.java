798b2ceca8230b57a0777f02494a894dfac952ae
Nathan Martin <nathan@noknt.net>
2024-08-09 13:07:38 -0400

    Add cache for PlayerService

--- plugin/velocity/src/main/java/group/aelysium/rustyconnector/plugin/velocity/lib/players/PlayerService.java
+++ plugin/velocity/src/main/java/group/aelysium/rustyconnector/plugin/velocity/lib/players/PlayerService.java
@@ -1,21 +1,19 @@
 package group.aelysium.rustyconnector.plugin.velocity.lib.players;
 
-import group.aelysium.rustyconnector.core.lib.cache.CacheableMessage;
 import group.aelysium.rustyconnector.plugin.velocity.central.Tinder;
 import group.aelysium.rustyconnector.toolkit.velocity.player.IPlayer;
 import group.aelysium.rustyconnector.toolkit.velocity.player.IPlayerService;
 import group.aelysium.rustyconnector.plugin.velocity.lib.storage.StorageService;
-import group.aelysium.rustyconnector.plugin.velocity.lib.storage.Database;
 
 import java.util.*;
 
 public class PlayerService implements IPlayerService {
-    private final Map<UUID, Boolean> recentPlayers;
+    private final Map<UUID, IPlayer> recentPlayers;
     private final StorageService storage;
 
     public PlayerService(StorageService storage) {
         this.storage = storage;
-        this.recentPlayers = new LinkedHashMap<>(100){
+        this.recentPlayers = new LinkedHashMap<>(500){
             @Override
             protected boolean removeEldestEntry(Map.Entry eldest) {
                 return this.size() > 100;
@@ -26,19 +24,38 @@ public class PlayerService implements IPlayerService {
     public void store(IPlayer player) {
         if(this.recentPlayers.containsKey(player.uuid())) return;
         this.storage.database().players().set(player);
-        this.recentPlayers.put(player.uuid(), false);
+        this.recentPlayers.put(player.uuid(), player);
     }
 
     public Optional<IPlayer> fetch(UUID uuid) {
-        return this.storage.database().players().get(uuid);
+        {
+            IPlayer recent = this.recentPlayers.get(uuid);
+            if (recent != null) return Optional.of(recent);
+        }
+        {
+            com.velocitypowered.api.proxy.Player velocityPlayer = Tinder.get().velocityServer().getPlayer(uuid).orElse(null);
+            if(velocityPlayer != null) return Optional.of(new Player(velocityPlayer));
+        }
+
+        Optional<IPlayer> player = this.storage.database().players().get(uuid);
+        player.ifPresent(p -> this.recentPlayers.put(p.uuid(), p));
+        return player;
     }
 
     public Optional<IPlayer> fetch(String username) {
-        return this.storage.database().players().get(username);
+        {
+            com.velocitypowered.api.proxy.Player velocityPlayer = Tinder.get().velocityServer().getPlayer(username).orElse(null);
+            if(velocityPlayer != null) return Optional.of(new Player(velocityPlayer));
+        }
+
+        Optional<IPlayer> player = this.storage.database().players().get(username);
+        player.ifPresent(p -> this.recentPlayers.put(p.uuid(), p));
+        return player;
     }
 
     @Override
     public void kill() {
         // this.storage.kill();  -  Storage is cleaned up in a different process.
+        this.recentPlayers.clear();
     }
 }
